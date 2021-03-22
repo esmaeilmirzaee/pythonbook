a3d2a08578ff1a25e8016f382ab46e09f231ec5f
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 09:49:05 -0400

    Get the input

---
+++ 04-variables/user-input-with-the-input-function.py
@@ -0,0 +1,6 @@
+# name = input('What is your name? ')
+# print(name)
+
+num_one = input('Enter a number: ')
+num_two = input('Enter a number: ')
+print('The sum of', num_one, 'and', num_two, 'is', float(num_one) + float(num_two))
