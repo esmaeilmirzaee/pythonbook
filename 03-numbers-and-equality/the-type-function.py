68cc03ceb32e624b1cc5933333f4e16f2a6e0487
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 05:09:45 -0400

    The type function

---
+++ 03-numbers-and-equality/the-type-function.py
@@ -0,0 +1,17 @@
+# An object is an instance of a class
+# Class: a blueprint from which objects are made.
+# Instance: an object made from a class.
+print(type(13))
+print(type(10000000))
+
+print(type(-1))
+
+print(type(13.0))
+
+print(type('computer'))
+print(type('c'))
+
+print(type(5) == type(13))
+print(type(True))
+
+print(type('True') == type(True))
