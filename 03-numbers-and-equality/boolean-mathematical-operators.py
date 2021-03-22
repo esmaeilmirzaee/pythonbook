964201aa798d66d838b7f825b5568d88066c487c
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 04:45:25 -0400

    equality operators

---
+++ 03-numbers-and-equality/boolean-mathematical-operators.py
@@ -0,0 +1,14 @@
+print(5 < 10)
+print(10 < 5)
+print(5 <= 5)
+print(8 <= 7)
+
+print(9 > 3)
+print(3 > 9)
+print(6 >= 1)
+print(1 >= 1)
+print(1 >= 2)
+
+print(10 < -1 > 2)
+print(11 < 12 > -1)
+print(-1 < 2 < 5 <= 8)
