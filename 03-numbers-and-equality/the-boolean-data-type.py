4b68cddcd477a064a91cf107e8471b39fb5c0847
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 02:03:59 -0400

    The boolean data type

---
+++ 03-numbers-and-equality/the-boolean-data-type.py
@@ -0,0 +1,29 @@
+# Boolean: a data type that can be either true or false.
+print(True)
+print(False)
+
+print('True')
+print('False')
+
+print(5 == 5)
+print(3.0 == 3)
+
+print(1 == 2)
+print('1' == 1)
+print('1 ' == '1 ')
+print(" " == ' ')
+
+print(3.10 == 3.1)
+
+print(True == True)
+print(False == True)
+print(False == False)
+
+# != not equal
+# == equal
+print(True != False)
+print('Man' != 'man')
+print('man' != 'man')
+print(10 != '10')
+print(10 != 10.0)
+print(10 != 10)
