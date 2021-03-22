55aa7e27230b044ce55e12359351f409790c70ac
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 06:47:58 -0400

    Intro to variables

---
+++ 04-variables/intro-to-variables.py
@@ -0,0 +1,24 @@
+# Variable: a name assigned to an object
+name = 'Esmaeil MIRZAEE'
+eyes_colour = 'Blue'
+age = 99
+email = 'EsmaeilMIRZAEE@example.com'
+
+print(name)
+print(age + 1)
+print(age)
+print('Hi! This is', name)
+
+
+age = 10
+print(age)
+
+# Do not do these; They are possible but not expected!
+age = 'Age'
+print(age)
+
+age = True
+print(age)
+
+age = 99
+print(age)
