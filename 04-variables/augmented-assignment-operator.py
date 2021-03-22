0b1cd80a2672f99546eb4473c96696d666446ff5
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 08:01:42 -0400

    Augmented assignment

---
+++ 04-variables/augmented-assignment-operator.py
@@ -0,0 +1,26 @@
+a = b = c = d = 10
+a = a + 3
+print(a)
+
+a += 1
+print(a)
+
+b -= 2
+print(b)
+
+c *= 3
+print(c)
+
+d /= 2
+print(d)
+
+d %= 5
+print(d)
+
+word = 'Ballon'
+word = 'Red' + word
+print(word)
+
+word = 'Red'
+word += 'Ballon'
+print(word)
