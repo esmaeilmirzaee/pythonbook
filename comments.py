b5b40a86116116f18d49736b04f7e6502f375d56
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-21 23:06:37 -0400

    Comments

---
+++ comments.py
@@ -0,0 +1,7 @@
+# Comment: a line of text is ignored by Python
+print('Good morning')
+# print('Good afternoon')
+print('Good night')
+
+# The following line does a math operation
+print(1 + 1) # prints 2
