0d2f1f9f273704a3c4871678766018a2aec8bb55
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-21 22:23:19 -0400

    Function

---
+++ 02-welcome-to-python/objects-srings.py
@@ -0,0 +1,28 @@
+// Literal: a piece of syntax that creates an object 
+// String: an immutable sequence of text characters
+// Text characters: all the character you can type with your keyboard
+// Immutable == Incapable
+
+"sushi"
+'sushi'
+
+"warriorfans1231"
+
+"$5.00"
+
+"You're right"
+'Shakespeare once wrote "To be or not to be" in one of his plays'
+
+" "
+""
+''
+
+"5"
+5
+
+
+// Documentation strings
+"""Hello
+my name is
+Esmaeil
+"""
---
+++ 02-welcome-to-python/the-print-function-1-single-argument.py
@@ -0,0 +1,3 @@
+// A sequence of one or more steps, a procedure, a routine, a series of instructions
+// A collection of organised Python code that can be reused
+// Two types of functions: 1. built-in: print, len, sum, etc. 2. custom functions
