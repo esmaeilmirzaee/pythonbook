42505291d6526b12efa0f93c47253a7ac61a65a8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 01:23:30 -0400

    PEMDAS

---
+++ 03-numbers-and-equality/mathematical-expressions.py
@@ -0,0 +1,19 @@
+# Operator: a symbol that carries out an operation
+# Operand: 3 and 4
+print(3 + 4)
+print(4.2 + 9.1)
+print(10 - 2)
+print(12 / 3)
+print(6 * 9)
+
+print('Ba' + "lon")
+print('Ha' * 4)
+
+# more specific
+print(9 - 3.2)
+print(9 + 1.9)
+
+print(5 ** 3)
+print(2 ** 3)
+
+# PEMDAS: Paranthesis, Exponents, Multiplication, Division, Addition, Subtraction
