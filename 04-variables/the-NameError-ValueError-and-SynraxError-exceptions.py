a1e52d5ac4aa156a25d7d3ac93d52fd6db59a9e5
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 10:29:23 -0400

    Some sample errors and exceptions

---
+++ 04-variables/the-NameError-ValueError-and-SynraxError-exceptions.py
@@ -0,0 +1,21 @@
+# Exception: an error in the program
+# Traceback: a summary or log of the error
+
+# NameError
+# first_name = 'Esmaeil'
+# print(firstname)
+
+# TypeError
+# TypeError: can only concatenate str (not "int") to str
+# print('5' + 3)
+
+# ValueError
+# ValueError: invalid literal for int() with base 10: 'a'
+# int('a')
+
+# SyntaxError
+# SyntaxError: EOL while scanning string literal
+# print('Hello)
+
+# SyntaxError: unexpected EOF while parsing
+# print(2 + 3
