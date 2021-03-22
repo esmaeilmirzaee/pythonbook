194bee1fc0cfe728b3c8b70a1df0f2c59b190e46
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-22 10:43:47 -0400

    Intro to functions

---
+++ 05-functions/intro-to-function.py
@@ -0,0 +1,7 @@
+# Function: A collection of one or more statements
+def welcome_message():
+  print('Welcome to programming in Python.')
+  print('Have a nice day programming.')
+
+
+welcome_message()
