f90707a97c1a25110d03201fc4cb93ab8995a644
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 04:43:41 -0400

    Slice strings

---
+++ 06-strings-the-basics/string-slicing-1-slicing-by-range.py
@@ -0,0 +1,10 @@
+address = 'Apple inc, Numero Un Bolivar, Capucinaco, California'
+
+print(address[:])
+print(address[0:])
+print(address[:-1])
+print(address[12:13])
+print(address[11:17])
+
+print(address[-10:-1])
+print(address[-10:])
