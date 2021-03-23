899c259f2e33e509e90b7c5d3960d2fb7293e566
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 03:11:01 -0400

    Index string

---
+++ 06-strings-the-basics/string-indexing-with-positive-values.py
@@ -0,0 +1,12 @@
+language = 'English'
+
+print(len(language))
+print(language[0])
+print(language[1])
+print(language[len(language) - 1])
+
+# IndexError
+# print(language[len(language)])
+
+# TypeError
+# language[0] = 'e'
