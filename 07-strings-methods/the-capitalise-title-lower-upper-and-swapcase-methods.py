91e0cd638ef065a7f9ebb6801d531fa13d3017e1
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 17:44:03 +0430

    Methods for strings

---
+++ 07-strings-methods/the-capitalise-title-lower-upper-and-swapcase-methods.py
@@ -0,0 +1,11 @@
+print('Once upon a time'.capitalize())
+print('Once upon a time'.upper())
+print('Once upon a time'.lower())
+print('Once upon a time'.swapcase())
+
+sentence = 'To Be or Not to be'
+print(sentence.lower())
+print(sentence)
+
+sentence = sentence.title()
+print(sentence)
