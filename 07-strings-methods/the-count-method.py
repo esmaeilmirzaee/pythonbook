bc7c006492696bfc4f6b5dd42cd43545e701710e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 07:52:02 -0400

    count method

---
+++ 07-strings-methods/the-count-method.py
@@ -0,0 +1,9 @@
+name = 'Esmaeil MIRZAEE'
+
+print(name.count('e'))
+print(name.count('E'))
+print(name.count('mi'))
+print(name.count('Mi'))
+print(name.count('MI'))
+print(name.count('sm'))
+print(name.count('e') + name.count('E'))
