56b4040b6ffb1dd777d21cdcae29a7454fcaec06
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 01:50:21 -0400

    length of strings

---
+++ 06-strings-the-basics/length-concatenations-and-immutability.py
@@ -0,0 +1,17 @@
+print(len(" "))
+print(len(""))
+print(len("Hi"))
+
+print(len('4'))
+# print(len(4))
+
+print(len('3.14'))
+# print(len(3.14))
+
+print("Esmaeil", 'MIRZAEE')
+print("Esmaeil" 'MIRZAEE')
+print("Esmaeil" " " 'MIRZAEE')
+print("Esmaeil" + 'MIRZAEE')
+print("Esmaeil" + ' MIRZAEE')
+print("Esmaeil " + 'MIRZAEE')
+print("Esmaeil" + ' ' + 'MIRZAEE')
