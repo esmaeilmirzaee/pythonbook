b5bb2940fa721b3522b96484507abe0097142a34
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 07:46:48 -0400

    startswith and endswith methods

---
+++ 07-strings-methods/the-startswith-and-endswith-methods.py
@@ -0,0 +1,15 @@
+sentence = 'To be or not to be'
+
+print(sentence.startswith('T'))
+print(sentence.startswith('To'))
+print(sentence.startswith('to'))
+print(sentence.startswith('To B'))
+print(sentence.startswith('To b'))
+print(sentence.startswith(''))
+
+print(sentence.endswith('e'))
+print(sentence.endswith('To be'))
+print(sentence.endswith('tobe'))
+print(sentence.endswith('to be'))
+print(sentence.endswith('be'))
+
