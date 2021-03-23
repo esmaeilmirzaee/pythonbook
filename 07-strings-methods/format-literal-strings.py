3f2333b0c3842086c484e0fdde9b4f2c4c84e69d
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 18:43:32 +0430

    Format literals

---
+++ 07-strings-methods/format-literal-strings.py
@@ -0,0 +1,9 @@
+name = 'Esmaeil MIRZAEE'
+first_section = 'privacy'
+second_section = 'licence'
+agreement = f'{name} is agreed to follow the {first_section} and {second_section} agreement.'
+
+print(agreement)
+
+print(f'2 + 2 is {2 + 2}')
+print(F'2 * 2 is {2 * 2}')
