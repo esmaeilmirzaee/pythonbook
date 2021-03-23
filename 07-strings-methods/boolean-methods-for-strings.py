fc32b13132dc4af92f4c11b402b4ea9b5184b31e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-23 17:55:24 +0430

    Check strings

---
+++ 07-strings-methods/boolean-methods-for-strings.py
@@ -0,0 +1,22 @@
+print('a'.islower())
+print('A'.islower())
+
+print('a'.isupper())
+print('A'.isupper())
+
+print('A Book'.istitle())
+print('a Book'.istitle())
+
+print('abcdef'.isalpha())
+print('abcdef1'.islapha())
+print('acb1'.islapha())
+
+print('123'.isnumeric())
+print(123.isnumberic())
+print('12!@'.isnumberic())
+
+print('Numero 1'.isalnum())
+print('Numero1'.isalnum())
+
+print(' '.isspace())
+print(''.isspace())
