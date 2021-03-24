a433a5688f3c70c04c6746c0edf2919934e7d9c6
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-24 15:45:00 +0430

    Review boolean data type

---
+++ 08-control-flow/REVIEW-the-boolean-data-type-equality-and-inequality.py
@@ -0,0 +1,18 @@
+# Control flow: the order in which individual statements, instructions, or function calls of a program are executed.
+
+male = True
+graduated = True
+print(male, graduated)
+print(male == graduated)
+
+print(2 <= 3)
+print(13 >= 13)
+print(1 >= 13)
+print("a" == 'a')
+print('a' == 'A')
+print("" == ' ')
+
+print(" " != ' ')
+print("E" != 'e')
+print(False != True)
+print(True != True)
