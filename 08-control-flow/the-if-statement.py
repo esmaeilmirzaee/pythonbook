096fd2a7bd5e44b150aef1d8de58261bb884752f
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-24 16:03:26 +0430

    The if statement

---
+++ 08-control-flow/the-if-statement.py
@@ -0,0 +1,17 @@
+if 'A' == 'A':
+    print('Of course, "A" is equal to "A"')
+
+if 'a' != 'a':
+    print("Sorry :(")
+
+if True:
+    print("Yep")
+
+if False:
+    print("Nope")
+
+if 'True':
+    print('True')
+
+if '':
+    print('Blank')
