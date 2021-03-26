a6d8652a2582a29ec902c8ae188915d29512fb58
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-26 21:49:48 +0430

    Another sample for while loop

--- 08-control-flow/the-while-loop.py
+++ 08-control-flow/the-while-loop.py
@@ -4,3 +4,17 @@ count = 0
 while count < 100:
   print(count)
   count+=10
+
+correct = True
+
+secret_number = random(1, 100)
+
+while correct:
+  print(secret_number)
+  a = int(input("Guess the number"))
+  if a == secret_number:
+    print("You guessed it.")
+    correct = False
+  else:
+    print("Try again")
+
