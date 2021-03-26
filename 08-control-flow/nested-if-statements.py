6c4649e4ccdcd6ab9cf05f0be9d45d71b38477a9
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-26 21:28:10 +0430

    While and nested condition

---
+++ 08-control-flow/nested-if-statements.py
@@ -0,0 +1,10 @@
+weather = 'rainy'
+sky = 'cloudy'
+
+if weather == 'rainy' and sky == 'cloudy':
+  print("Get your umbrella")
+
+if weather == 'rainy':
+  if sky == 'cloudy':
+    print("Get your coat")
+
---
+++ 08-control-flow/the-while-loop.py
@@ -0,0 +1,6 @@
+# loop: a series of instructions that is repeated.
+count = 0
+
+while count < 100:
+  print(count)
+  count+=10
