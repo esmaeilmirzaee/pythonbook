461270605c18b9e01ca5b834941068a37a3f13b9
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-03-26 21:21:31 +0430

    More sample for not

--- 08-control-flow/the-not-keyword.py
+++ 08-control-flow/the-not-keyword.py
@@ -6,3 +6,12 @@ if 1 < 100:
 
 if 1 >= 100:
   print('1 is less than 100')
+
+if 1 < 100:
+  print("This is going to be printed.")
+
+if 1 > 100:
+  print("This won't be printed.")
+
+if not 1 > 100:
+  print("This is going to be printed.")
